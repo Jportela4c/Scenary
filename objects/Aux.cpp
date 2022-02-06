b6a4a3aa7047bf50cf47403ebf63216d204fa8d4
pedrosfa <sfa.pedro@gmail.com>
2022-02-06 20:57:11 -0300

    cube corrections4

--- objects/Aux.cpp
+++ objects/Aux.cpp
@@ -31,14 +31,14 @@ Face::Face(Vertex vertex1, Vertex vertex2, Vertex vertex3)
     this->v1v3 = this->v3 - this->v1;
     this->v2v3 = this->v3 - this->v2;
     this->v3v1 = this->v1 - this->v3;
-    v1v2[3] = 1;
-    v1v3[3] = 1;
-    v2v3[3] = 1;
-    v3v1[3] = 1;
+    this->v1v2[3] = 1;
+    this->v1v3[3] = 1;
+    this->v2v3[3] = 1;
+    this->v3v1[3] = 1;
 
     Point v1v3n = Point(v1v3[0], v1v3[1], v1v3[2]);
     Point v1v2n = Point(v1v2[0], v1v2[1], v1v2[2]);
-    Point normal = (v1v3n.cross(v1v2n)).normalized();
+    Point normal = (v1v2n.cross(v1v3n)).normalized();
 
     this->normal = Vertex(normal[0], normal[1], normal[2], 1.0);
 }
@@ -72,29 +72,38 @@ Vertex Face::rayIntersection(Vertex rayOrigin, Vertex rayDirection)
     {
         return Vertex(0,0,0,-1);
     }
-    Vertex pv1 = this->v1 - planeIntersection;
-    Vertex pv2 = this->v2 - planeIntersection;
-    Vertex pv3 = this->v3 - planeIntersection;
+    
+    Point pi = Point(planeIntersection[0], planeIntersection[1], planeIntersection[2]);
 
-    Point pv1n = Point(pv1[0], pv1[1], pv1[2]);
-    Point pv2n = Point(pv2[0], pv2[1], pv2[2]);
-    Point pv3n = Point(pv3[0], pv3[1], pv3[2]);
+    Point v1p = Point(this->v1[0], this->v1[1], this->v1[2]);
+    Point v2p = Point(this->v2[0], this->v2[1], this->v2[2]);
+    Point v3p = Point(this->v3[0], this->v3[1], this->v3[2]);
 
-    Point v1v2n = Point(this->v1v2[0], this->v1v2[1], this->v1v2[2]);
-    Point v1v3n = Point(this->v1v3[0], this->v1v3[1], this->v1v3[2]);
-    Point v2v3n = Point(this->v2v3[0], this->v2v3[1], this->v2v3[2]);
-    Point v3v1n = Point(this->v3v1[0], this->v3v1[1], this->v3v1[2]);
+    Point p1p2p = v2p - v1p;
+    Point p1p3p = v3p - v1p;
+    Point p2p3p = v3p - v2p;
+    Point p3p1p = v1p - v3p;
 
-    Point normaln = Point(this->normal[0], this->normal[1], this->normal[2]);
+    Point p1pi = pi - v1p;
+    Point p2pi = pi - v2p;
+    Point p3pi = pi - v3p;
 
-    float e = (pv1n.cross(pv2n)).dot(normaln);
-    float f = (pv2n.cross(pv3n)).dot(normaln);
-    float s = 1 - e - f;
-    if (e < 0)
+    Point normalp = (p1p2p.cross(p1p3p)).normalized();
+    float N = (p1p2p.cross(p1p3p)).norm();
+
+    int se = (p1p2p.cross(p1pi)).dot(p1p2p.cross(p1p3p)) > 0 ? 1 : -1;
+    int sn = (p2p3p.cross(p2pi)).dot(p1p2p.cross(p1p3p)) > 0 ? 1 : -1;
+    int ss = (p3p1p.cross(p3pi)).dot(p1p2p.cross(p1p3p)) > 0 ? 1 : -1;
+
+    float e = se*(p1p2p.cross(p1pi).dot(normalp)/ N);
+    float n = sn*(p2p3p.cross(p2pi).dot(normalp)/ N);
+    float s = ss*(p3p1p.cross(p3pi).dot(normalp)/ N);
+
+    if(e < 0)
     {
         return Vertex(0,0,0,-1);
     }
-    else if (f < 0)
+    else if (n < 0)
     {
         return Vertex(0,0,0,-1);
     }
@@ -106,6 +115,40 @@ Vertex Face::rayIntersection(Vertex rayOrigin, Vertex rayDirection)
     {
         return planeIntersection;
     }
+    // Vertex pv1 = this->v1 - planeIntersection;
+    // Vertex pv2 = this->v2 - planeIntersection;
+    // Vertex pv3 = this->v3 - planeIntersection;
+
+    // Point pv1n = Point(pv1[0], pv1[1], pv1[2]);
+    // Point pv2n = Point(pv2[0], pv2[1], pv2[2]);
+    // Point pv3n = Point(pv3[0], pv3[1], pv3[2]);
+
+    // Point v1v2n = Point(this->v1v2[0], this->v1v2[1], this->v1v2[2]);
+    // Point v1v3n = Point(this->v1v3[0], this->v1v3[1], this->v1v3[2]);
+    // Point v2v3n = Point(this->v2v3[0], this->v2v3[1], this->v2v3[2]);
+    // Point v3v1n = Point(this->v3v1[0], this->v3v1[1], this->v3v1[2]);
+
+    // Point normaln = Point(this->normal[0], this->normal[1], this->normal[2]);
+
+    // float e = (pv1n.cross(pv2n)).dot(normaln);
+    // float f = (pv2n.cross(pv3n)).dot(normaln);
+    // float s = 1 - e - f;
+    // if (e < 0)
+    // {
+    //     return Vertex(0,0,0,-1);
+    // }
+    // else if (f < 0)
+    // {
+    //     return Vertex(0,0,0,-1);
+    // }
+    // else if (s < 0)
+    // {
+    //     return Vertex(0,0,0,-1);
+    // }
+    // else
+    // {
+    //     return planeIntersection;
+    // }
 };
 
 Vertex Face::getNormal()
--- objects/Cube.cpp
+++ objects/Cube.cpp
@@ -14,18 +14,31 @@ Cube::Cube(float side, Vertex  center, Material mat): s(side), c(center), mat(ma
     this->vertices[6] = Vertex(c[0] + s, c[1] - s, c[2] - s, 1);
     this->vertices[7] = Vertex(c[0] + s, c[1] - s, c[2] + s, 1);
 
-    this->faces[0] = Face(this->vertices[2], this->vertices[3], this->vertices[7]);
-    this->faces[1] = Face(this->vertices[2], this->vertices[7], this->vertices[6]);
-    this->faces[2] = Face(this->vertices[2], this->vertices[1], this->vertices[0]);
-    this->faces[3] = Face(this->vertices[2], this->vertices[0], this->vertices[3]);
-    this->faces[4] = Face(this->vertices[3], this->vertices[0], this->vertices[7]);
-    this->faces[5] = Face(this->vertices[0], this->vertices[4], this->vertices[7]);
-    this->faces[6] = Face(this->vertices[1], this->vertices[4], this->vertices[0]);
-    this->faces[7] = Face(this->vertices[1], this->vertices[5], this->vertices[4]);
-    this->faces[8] = Face(this->vertices[7], this->vertices[4], this->vertices[6]);
+    // this->faces[0] = Face(this->vertices[2], this->vertices[3], this->vertices[7]);
+    // this->faces[1] = Face(this->vertices[2], this->vertices[7], this->vertices[6]);
+    // this->faces[2] = Face(this->vertices[2], this->vertices[1], this->vertices[0]);
+    // this->faces[3] = Face(this->vertices[2], this->vertices[0], this->vertices[3]);
+    // this->faces[4] = Face(this->vertices[3], this->vertices[0], this->vertices[7]);
+    // this->faces[5] = Face(this->vertices[0], this->vertices[4], this->vertices[7]);
+    // this->faces[6] = Face(this->vertices[1], this->vertices[4], this->vertices[0]);
+    // this->faces[7] = Face(this->vertices[1], this->vertices[5], this->vertices[4]);
+    // this->faces[8] = Face(this->vertices[7], this->vertices[4], this->vertices[6]);
+    // this->faces[9] = Face(this->vertices[4], this->vertices[5], this->vertices[6]);
+    // this->faces[10] = Face(this->vertices[2], this->vertices[6], this->vertices[1]);
+    // this->faces[11] = Face(this->vertices[6], this->vertices[5], this->vertices[1]);
+
+    this->faces[0] = Face(this->vertices[0], this->vertices[3], this->vertices[2]);
+    this->faces[1] = Face(this->vertices[2], this->vertices[1], this->vertices[0]);
+    this->faces[2] = Face(this->vertices[6], this->vertices[6], this->vertices[2]);
+    this->faces[3] = Face(this->vertices[5], this->vertices[1], this->vertices[2]);
+    this->faces[4] = Face(this->vertices[7], this->vertices[6], this->vertices[3]);
+    this->faces[5] = Face(this->vertices[6], this->vertices[2], this->vertices[3]);
+    this->faces[6] = Face(this->vertices[4], this->vertices[7], this->vertices[3]);
+    this->faces[7] = Face(this->vertices[3], this->vertices[0], this->vertices[4]);
+    this->faces[8] = Face(this->vertices[6], this->vertices[7], this->vertices[4]);
     this->faces[9] = Face(this->vertices[4], this->vertices[5], this->vertices[6]);
-    this->faces[10] = Face(this->vertices[2], this->vertices[6], this->vertices[1]);
-    this->faces[11] = Face(this->vertices[6], this->vertices[5], this->vertices[1]);
+    this->faces[10] = Face(this->vertices[0], this->vertices[5], this->vertices[4]);
+    this->faces[11] = Face(this->vertices[5], this->vertices[0], this->vertices[1]);
 
     this->normal_intersect = Vertex(0, 0, 0, -1);
 
